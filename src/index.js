eea75c4cce2cc9961117dbe5f4d59e758593fa85
Roparat Sukapirom <roparat@gmail.com>
2019-06-06 14:05:17 +0700

    moving button binding to index.js, add config update setting

--- src/index.js
+++ src/index.js
@@ -11,3 +11,22 @@ const drawOnce = type =>
     type();
     pop();
   };
+
+window.onload = function() {
+  const drawV1Button = document.getElementById('drawV1Button');
+  drawV1Button.addEventListener('click', drawOnce(V1));
+  const drawV2Button = document.getElementById('drawV2Button');
+  drawV2Button.addEventListener('click', drawOnce(V2));
+  const drawV3Button = document.getElementById('drawV3Button');
+  drawV3Button.addEventListener('click', drawOnce(V3));
+  const updateSettingButton = document.getElementById('update');
+  updateSettingButton.addEventListener('click', () => {
+    const widthInput = document.getElementById('width');
+    const heightInput = document.getElementById('height');
+    const colorInput = document.getElementById('color');
+    CANVAS_SIZE.WIDTH = parseInt(widthInput.value, 10);
+    CANVAS_SIZE.HEIGHT = parseInt(heightInput.value, 10);
+    BACKGROUND_COLOR = colorInput.value;
+    setup();
+  });
+};
